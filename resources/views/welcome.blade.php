d99e515e66c5da55a0f80412308f0db20e7aca5b
Habib <habib.abdurrahman00@gmail.com>
2020-09-03 19:58:39 +0700

    ubah family font jadi Source Sans Pro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
@@ -15,8 +15,9 @@
                 padding: 0;
                 width: 100%;
                 display: table;
-                font-weight: 100;
-                font-family: 'Lato';
+                font-weight: lighter;
+                font-family: 'Source Sans Pro';
+                font-style: italic;
             }
 
             .container {
